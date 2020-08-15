96d2abf59a2b26071699242e5622c818a511932e
Jonas Klein <workstation@jonasklein.dev>
2020-08-15 19:23:15 +0200

    Make it work

--- src/main.rs
+++ src/main.rs
@@ -16,26 +16,17 @@ use renderer::base::Vertex;
 
 use specs::prelude::*;
 
-enum RendererState {
-    Unitialized,
-    Initialized {
-        instance: wgpu::Instance,
-        size: winit::dpi::PhysicalSize<u32>,
-        surface: wgpu::Surface,
-        adapter: wgpu::Adapter,
-        device: wgpu::Device,
-        queue: wgpu::Queue,
-        sc_desc: wgpu::SwapChainDescriptor,
-        swap_chain: wgpu::SwapChain,
-        objects: std::vec::Vec<StaticMeshObject>,
-        object_types : std::vec::Vec<RenderObject> 
-    }
-}
-
-impl Default for RendererState {
-    fn default() -> Self {
-        RendererState::Unitialized
-    }
+struct RendererState {
+    instance: wgpu::Instance,
+    size: winit::dpi::PhysicalSize<u32>,
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    sc_desc: wgpu::SwapChainDescriptor,
+    swap_chain: wgpu::SwapChain,
+    objects: std::vec::Vec<StaticMeshObject>,
+    object_types : std::vec::Vec<RenderObject> 
 }
 
 impl RendererState {
@@ -112,7 +103,7 @@ impl RendererState {
 
         let static_mesh_type = StaticMesh::new(&device, sc_desc.format);
 
-        let t = RendererState::Initialized {
+        RendererState {
             instance,
             adapter,
             size,
@@ -125,12 +116,10 @@ impl RendererState {
             object_types: vec![
                 RenderObject::StaticMesh(static_mesh_type)
             ]
-        };
-
-        t
+        }
     }
 
-    fn render(&mut self) {
+    /*fn render(&mut self) {
         match self {
             RendererState::Initialized {
                 instance,
@@ -186,9 +175,9 @@ impl RendererState {
             },
             _ => panic!("Render call on uninitialized RenderState!")
         }
-    }
+    }*/
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+   /* fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         match self {
             RendererState::Initialized {
                 surface,
@@ -202,132 +191,15 @@ impl RendererState {
             },
             _ => panic!("Cannot resize unitialized RenderState!")
         }
-    }
+    }*/
 }
 
 struct Renderer;
 
-impl <'a> System<'a> for Renderer {
-    type SystemData = Write<'a, RendererState>;
-
-    fn run(&mut self, state: Self::SystemData) {
-        match *state {
-            RendererState::Initialized {..} => {
-                state.render();
-            },
-            RendererState::Unitialized => {
-                *state = RendererState::new(window: &winit::window::Window);  
-            }
-        }
-    }
-}
-
-struct State {
-    instance: wgpu::Instance,
-    size: winit::dpi::PhysicalSize<u32>,
-    surface: wgpu::Surface,
-    adapter: wgpu::Adapter,
-    device: wgpu::Device,
-    queue: wgpu::Queue,
-    sc_desc: wgpu::SwapChainDescriptor,
-    swap_chain: wgpu::SwapChain,
-    objects: std::vec::Vec<StaticMeshObject>,
-    object_types : std::vec::Vec<RenderObject>
-}
-
-impl State {
-    async fn new(window: &winit::window::Window) -> Self {
-        
-        let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
-        let (size, surface) = unsafe {
-            let size = window.inner_size();
-            let surface = instance.create_surface(window);
-            (size, surface)
-        };
-    
-        let adapter = instance
-            .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference: wgpu::PowerPreference::Default,
-                compatible_surface: Some(&surface),
-            })
-            .await
-            .unwrap();
-    
-        // Todo: Specify required features
-        let adapter_features = adapter.features();
-        log::info!("Features: {:?}", adapter_features);
-    
-        // Todo: Specify limits
-        let limits = wgpu::Limits {
-            max_bind_groups: 6,
-            ..wgpu::Limits::default()
-        };
-        log::info!("Limits: {:#?}", limits);
-    
-        let trace_dir = std::env::var("WGPU_TRACE");
-    
-        let (device, queue) = adapter
-            .request_device(
-                &wgpu::DeviceDescriptor {
-                    features: wgpu::Features::empty(),
-                    limits: limits,
-                    shader_validation: true,
-                },
-                trace_dir.ok().as_ref().map(std::path::Path::new),
-            )
-            .await
-            .unwrap();
-
-        let sc_desc = wgpu::SwapChainDescriptor {
-            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-            format: wgpu::TextureFormat::Bgra8Unorm,
-            width: size.width,
-            height: size.height,
-            present_mode: wgpu::PresentMode::Mailbox,
-        };
-
-        let swap_chain = device.create_swap_chain(&surface, &sc_desc);
-
-        let triangle = StaticMeshObject::new(
-            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: None,
-                contents: bytemuck::cast_slice(&[
-                    Vertex::new([0.0, 0.5, 0.0], [1.0, 0.0, 0.0]),
-                    Vertex::new([-0.5, -0.5, 0.0], [0.0, 1.0, 0.0]),
-                    Vertex::new([0.5, -0.5, 0.0], [0.0, 0.0, 1.0]),
-                ]),
-                usage: wgpu::BufferUsage::VERTEX
-            }),
-            device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-                label: None,
-                contents: bytemuck::cast_slice(&[
-                    0 as u16,1,2
-                ]),
-                usage: wgpu::BufferUsage::INDEX
-            })
-        );
-
-        let static_mesh_type = StaticMesh::new(&device, sc_desc.format);
-
-        State {
-            instance,
-            adapter,
-            size,
-            surface,
-            device,
-            queue,
-            sc_desc,
-            swap_chain,
-            objects: vec![triangle],
-            object_types: vec![
-                RenderObject::StaticMesh(static_mesh_type)
-            ]
-        }
-    }
-
-    fn render(&mut self) {
-        let screen_frame = self.swap_chain.get_current_frame().expect("Failed to acquire next swap chain texture!");
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+impl Renderer {
+    fn render(RendererState { device, swap_chain, queue, objects, object_types, ..}: &mut RendererState) {
+        let screen_frame = swap_chain.get_current_frame().expect("Failed to acquire next swap chain texture!");
+        let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
             label: None
         });
 
@@ -351,10 +223,10 @@ impl State {
                 depth_stencil_attachment: None
             });
 
-            let mut objects = self.objects.iter();
+            let mut objects = objects.iter();
             
             while let Some(object) = objects.next() {
-                let object_type = self.object_types.get(object.object_type()).unwrap();
+                let object_type = object_types.get(object.object_type()).unwrap();
                 render_pass.set_pipeline(object_type.get_pipeline());
                 render_pass.set_vertex_buffer(0, object.vertex_buffer());
                 render_pass.set_index_buffer(object.index_buffer());
@@ -362,19 +234,24 @@ impl State {
             }
         }
 
-        self.queue.submit(std::iter::once(encoder.finish()));
-    }
+        queue.submit(std::iter::once(encoder.finish()));
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        self.sc_desc.width = new_size.width;
-        self.sc_desc.height = new_size.height;
-        self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    }
+}
+
+impl <'a> System<'a> for Renderer {
+    type SystemData = WriteExpect<'a, RendererState>;
+
+    fn run(&mut self, mut data: Self::SystemData) {
+        Self::render(&mut *data);
     }
 }
 
 fn main() {
 
     env_logger::init();
+
+    let mut world = World::new();
 
     let (mut pool, _spawner) = {
         let local_pool = futures::executor::LocalPool::new();
@@ -388,7 +265,10 @@ fn main() {
     let window_builder = winit::window::WindowBuilder::new();
     let window = window_builder.with_title("Cells").build(&event_loop).unwrap();
 
-    let mut state = futures::executor::block_on(State::new(&window));
+    let renderer_state = futures::executor::block_on(RendererState::new(&window));
+    world.insert(renderer_state);
+
+    let mut dispatcher = DispatcherBuilder::new().with(Renderer, "renderer", &[]).build();
 
     event_loop.run(move |event, _, control_flow| {
 
@@ -416,7 +296,7 @@ fn main() {
                 ..
             } => {
                 log::info!("Resizing to {:?}", size);
-                state.resize(size);
+                //state.resize(size);
             }
             event::Event::WindowEvent { event, .. } => match event {
                 WindowEvent::KeyboardInput {
@@ -437,9 +317,12 @@ fn main() {
                 }
             },
             event::Event::RedrawRequested(_) => {
-                state.render();                
+                //state.render();                
             }
             _ => {}
         }
+
+        dispatcher.dispatch(&mut world);
+        world.maintain();
     });
 }
